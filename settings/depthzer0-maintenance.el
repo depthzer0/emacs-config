31bba0c0e13a484340d6f48afaa7be0024bf2bfd
depthzer0 <carngarn@gmail.com>
2026-06-17 17:35:26 +0300

    chore: ingnore autoinsert for org-roam

--- settings/depthzer0-maintenance.el
+++ settings/depthzer0-maintenance.el
@@ -15,7 +15,15 @@
   (setq auto-insert-query nil)
   (setq auto-insert-directory (locate-user-emacs-file "templates/"))
   (add-to-list 'auto-insert-alist '("\\.gitignore\\'" . "gitignore.template"))
-  (add-to-list 'auto-insert-alist '("\\.org\\'" . "org.template")))
+  (add-to-list 'auto-insert-alist '("\\.org\\'" . "org.template"))
+
+    ;; Учим autoinsert игнорировать файлы Org Roam
+  (defun my-auto-insert-ignore-roam (orig-fun &rest args)
+    "Отключает auto-insert для файлов базы знаний Org Roam."
+    (unless (and (buffer-file-name)
+                 (string-match-p "org-roam" (buffer-file-name)))
+      (apply orig-fun args)))
+  (advice-add 'auto-insert :around #'my-auto-insert-ignore-roam))
 
 ;; --- Поддержка русской раскладки для горячих клавиш ---
 (use-package reverse-im
